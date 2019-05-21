75d7b893c3b3e21a18c87a98056bbf17579e0d5b
zhonglvbo <44047954+zhonglvbo@users.noreply.github.com>
2019-05-21 23:11:49 +0800

    Add files via upload

---
+++ App/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_restful import Api, Resource
+import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/pro'
+app.config['SECRET_KEY'] = 'key'
+app.config['UPLOAD_FOLDER'] = os.getcwd() + '/uploads/'
+db = SQLAlchemy(app)
+Bootstrap(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from app.models import *
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
+from app.views import *
